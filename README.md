c68c0456944f9757af9f5562d9df7b9569b7d6bd
jaumet <jaume@nualart.cat>
2014-02-25 11:09:29 +1100

    Working on UClaims: first sliders versions

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-A bunch of tools to work with patent texts. This is an academic project started on August 2013.
-By the moment we cannot tell you more about it because we are in a brainstorming phase. We hope to give some info very soon. 
-In case you are involved in patents analysis & visualization, we will be happy to know about it, please let as a comment.
+A bunch of visualization ideas to work with patent texts. 
+
+This is an academic project started on August 2013.
+
+By the moment we cannot tell you more about it because we are in a brainstorming phase. We hope to publish some info very soon.
+In case you are involved in patents analysis and/or patents visualization, we will be happy to know about it, please let as a comment.
+
 Thanks for your interest in this project.
--- UPatent/data/4636v.js
+++ UPatent/data/4636v.js
@@ -1,5 +1,8 @@
-{
-    "classificationCodes": [
+DATA = 
+[
+  {
+    "classificationCodes": 
+      [
         {
             "sectionTitle": "electricity",
             "subgroupTitle": "means associated with receiver for limiting or suppressing noise or interference induced by transmission interference reduction in spread spectrum systems ; equalising on hf or if ; diversity systems ; elimination of image frequencies ; noise suppression by control of amplification , , ; squelching ,",
@@ -14,21 +17,6 @@
             "order": 1,
             "subclass": "B",
             "subgroup": "10"
-        },
-        {
-            "sectionTitle": "electricity",
-            "subgroupTitle": "diversity",
-            "maingroupTitle": "indexing scheme relating to details of transmission systems not covered by a single group of to",
-            "class": "04",
-            "codeType": "furtherCode",
-            "section": "H",
-            "subclassTitle": "transmission transmission systems for measured values, control or similar signals coding, decoding, code conversion, in general broadcast communication multiplex systems secret communication transmission of digital information",
-            "maingroup": "2201",
-            "classTitle": "electric communication technique",
-            "fullCode": "H04B220171632",
-            "order": 2,
-            "subclass": "B",
-            "subgroup": "71632"
         },
         {
             "sectionTitle": "electricity",
@@ -47,18 +35,33 @@
         },
         {
             "sectionTitle": "electricity",
-            "subgroupTitle": "diversity systems for direction finding ; aerial arrays or systems reducing multipath interference in spread spectrum systems ; specially adapted for satellite systems ; for telegraphy or data transmission",
-            "maingroupTitle": "radio transmission systems, i.e. using radiation field , take precedence",
+            "subgroupTitle": "diversity",
+            "maingroupTitle": "indexing scheme relating to details of transmission systems not covered by a single group of to",
             "class": "04",
-            "codeType": "mainCode",
+            "codeType": "furtherCode",
+            "section": "H",
+            "subclassTitle": "transmission transmission systems for measured values, control or similar signals coding, decoding, code conversion, in general broadcast communication multiplex systems secret communication transmission of digital information",
+            "maingroup": "2201",
+            "classTitle": "electric communication technique",
+            "fullCode": "H04B220171632",
+            "order": 2,
+            "subclass": "B",
+            "subgroup": "71632"
+        },
+        {
+            "sectionTitle": "electricity",
+            "subgroupTitle": "using phase diversity e.g. phase sweeping",
+            "maingroupTitle": "radio transmission systems, i.e. u4636.jsonsing radiation field , take precedence",
+            "class": "04",
+            "codeType": "furtherCode",
             "section": "H",
             "subclassTitle": "transmission transmission systems for measured values, control or similar signals coding, decoding, code conversion, in general broadcast communication multiplex systems secret communication transmission of digital information",
             "maingroup": "7",
             "classTitle": "electric communication technique",
-            "fullCode": "H04B702",
-            "order": 0,
+            "fullCode": "H04B70682",
+            "order": 5,
             "subclass": "B",
-            "subgroup": "02"
+            "subgroup": "0682"
         },
         {
             "sectionTitle": "electricity",
@@ -77,18 +80,18 @@
         },
         {
             "sectionTitle": "electricity",
-            "subgroupTitle": "using phase diversity e.g. phase sweeping",
+            "subgroupTitle": "diversity systems for direction finding ; aerial arrays or systems reducing multipath interference in spread spectrum systems ; specially adapted for satellite systems ; for telegraphy or data transmission",
             "maingroupTitle": "radio transmission systems, i.e. using radiation field , take precedence",
             "class": "04",
-            "codeType": "furtherCode",
+            "codeType": "mainCode",
             "section": "H",
             "subclassTitle": "transmission transmission systems for measured values, control or similar signals coding, decoding, code conversion, in general broadcast communication multiplex systems secret communication transmission of digital information",
             "maingroup": "7",
             "classTitle": "electric communication technique",
-            "fullCode": "H04B70682",
-            "order": 5,
+            "fullCode": "H04B702",
+            "order": 0,
             "subclass": "B",
-            "subgroup": "0682"
+            "subgroup": "02"
         },
         {
             "sectionTitle": "electricity",
@@ -105,10 +108,13 @@
             "subclass": "B",
             "subgroup": "0825"
         }
+      }
     ],
     "metaData": {
         "title": "us-patent-grant",
         "patNumber": "08503561",
         "country": "US"
     }
-}
+
+  }
+]
